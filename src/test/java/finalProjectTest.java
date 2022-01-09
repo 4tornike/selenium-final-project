cef86384164c2ea13e31042d31d7eb867e17a02e
4tornike <79340245+4tornike@users.noreply.github.com>
2022-01-09 22:16:01 +0400

    Update finalProjectTest.java

--- src/test/java/finalProjectTest.java
+++ src/test/java/finalProjectTest.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class finalProjectTest {
 
@@ -35,6 +38,7 @@ public class finalProjectTest {
         }
     }
 
+
     @Test
 
     public void register() {
@@ -44,18 +48,19 @@ public class finalProjectTest {
         myAccount.click();
         WebElement register = driver.findElement(By.xpath("//a[text()='Register']"));
         register.click();
+        js.executeScript("window.scrollBy(0,300)");
         WebElement firstName = driver.findElement(By.cssSelector("input[name='firstname']"));
         firstName.sendKeys("test");
         WebElement lastName = driver.findElement(By.id("input-lastname"));
         lastName.sendKeys("test");
         WebElement email = driver.findElement(By.xpath("//input[@placeholder='E-Mail' and @class='form-control']"));
-        email.sendKeys("test@gmail.com");
+        email.sendKeys("torniketest@gmail.com");
         WebElement telephone = driver.findElement(By.cssSelector("#input-telephone"));
         telephone.sendKeys("test");
-        WebElement password = driver.findElement(By.xpath("//*[contains(@name,'password')]"));
-        password.sendKeys("test");
+        WebElement password = driver.findElement(By.xpath("//*[contains(@name,'pass')]"));
+        password.sendKeys("secretpassword");
         WebElement confirmPassword = driver.findElement(By.name("confirm"));
-        confirmPassword.sendKeys("test");
+        confirmPassword.sendKeys("secretpassword");
         WebElement subscribe = driver.findElement(By.xpath("//*[@class='radio-inline']/input[@value='1']"));
         js.executeScript("arguments[0].checked=true",subscribe);
         WebElement agree = driver.findElement(By.cssSelector("input[name='agree']"));
@@ -90,6 +95,8 @@ public class finalProjectTest {
     public void task3() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.location= 'http://tutorialsninja.com/demo/index.php?route=product/product&path=24&product_id=29'");
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".thumbnails li:first-child")));
         WebElement phoneImage = driver.findElement(By.cssSelector(".thumbnails li:first-child"));
         phoneImage.click();
         WebElement text = driver.findElement(By.className("mfp-counter"));
@@ -97,7 +104,6 @@ public class finalProjectTest {
         String actualText = js.executeScript("return document.getElementsByClassName('mfp-counter')[0].innerHTML").toString();
         while(!expectedText.equalsIgnoreCase(actualText)) {
             WebElement image = driver.findElement(By.className("mfp-img"));
-            WebDriverWait wait = new WebDriverWait(driver,3);
             wait.until(ExpectedConditions.elementToBeClickable(image));
             image.click();
             actualText = js.executeScript("return document.getElementsByClassName('mfp-counter')[0].innerHTML").toString();
@@ -120,6 +126,7 @@ public class finalProjectTest {
         addToCart.click();
         WebElement shoppingCart = driver.findElement(By.xpath("//span[text()='Shopping Cart']"));
         shoppingCart.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='input-group btn-block']/input")));
         WebElement quantity = driver.findElement(By.xpath("//div[@class='input-group btn-block']/input"));
         String expectedQuantity = quantity.getAttribute("value").toString();
         WebElement total = driver.findElement(By.xpath("//strong[text()='Total']/parent::td/following-sibling::td"));
@@ -137,7 +144,127 @@ public class finalProjectTest {
         } else {
             System.out.println("quantity is not same");
         }
+        item.click();
+        WebElement checkout = driver.findElement(By.xpath("//i[@class='fa fa-share']/parent::strong"));
+        checkout.click();
     }
 
+    @Test
+
+    public void task4() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.location= 'http://tutorialsninja.com/demo/index.php?route=checkout/checkout'");
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("input-email")));
+        WebElement email = driver.findElement(By.id("input-email"));
+        email.sendKeys("torniketest@gmail.com");
+        WebElement password = driver.findElement(By.id("input-password"));
+        password.sendKeys("secretpassword");
+        WebElement login = driver.findElement(By.id("button-login"));
+        login.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[value='new']")));
+        WebElement newAddress = driver.findElement(By.cssSelector("input[value='new']"));
+        newAddress.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("button-payment-address")));
+        WebElement prolong1 = driver.findElement(By.id("button-payment-address"));
+        js.executeScript("arguments[0].scrollIntoView()",prolong1);
+        WebElement firstName = driver.findElement(By.name("firstname"));
+        firstName.sendKeys("test");
+        WebElement lastName = driver.findElement(By.name("lastname"));
+        lastName.sendKeys("test");
+        WebElement company = driver.findElement(By.name("company"));
+        company.sendKeys("test");
+        WebElement address1 = driver.findElement(By.name("address_1"));
+        address1.sendKeys("test");
+        WebElement address2 = driver.findElement(By.name("address_2"));
+        address2.sendKeys("test");
+        WebElement city = driver.findElement(By.name("city"));
+        city.sendKeys("test");
+        WebElement postcode = driver.findElement(By.name("postcode"));
+        postcode.sendKeys("1234");
+        WebElement country = driver.findElement(By.xpath("//select[@name='country_id']/option[@value='80']"));
+        country.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//select[@id='input-payment-zone']//option[@value='1244']")));
+        WebElement paymentcity = driver.findElement(By.xpath("//select[@id='input-payment-zone']//option[@value='1244']"));
+        paymentcity.click();
+//        wait.until(ExpectedConditions.elementToBeClickable(By.id("button-payment-address")));
+//        WebElement prolong1 = driver.findElement(By.id("button-payment-address"));
+        prolong1.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("button-shipping-address")));
+        WebElement prolong2 = driver.findElement(By.id("button-shipping-address"));
+        prolong2.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='shipping_method']/parent::label")));
+        WebElement shipping = driver.findElement(By.xpath("//input[@name='shipping_method']/parent::label"));
+        String flatShipping = shipping.getText().substring(21);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("button-shipping-method")));
+        WebElement prolong3 = driver.findElement(By.id("button-shipping-method"));
+        prolong3.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.name("agree")));
+        WebElement terms = driver.findElement(By.name("agree"));
+        js.executeScript("arguments[0].checked=true",terms);
+        WebElement prolong4 = driver.findElement(By.id("button-payment-method"));
+        prolong4.click();
+        WebElement cartTotal = driver.findElement(By.id("cart-total"));
+        cartTotal.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//strong[text()='Sub-Total']/parent::td/following-sibling::td")));
+        WebElement subTotal1 = driver.findElement(By.xpath("//strong[text()='Sub-Total']/parent::td/following-sibling::td"));
+        WebElement total1 = driver.findElement(By.xpath("//strong[text()='Total']/parent::td/following-sibling::td"));
+        String FirstsubTotalprice = subTotal1.getText().toString();
+        String firsttotalprice = total1.getText().toString();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='Sub-Total:']/parent::td/following-sibling::td")));
+        WebElement subTotal2 = driver.findElement(By.xpath("//*[text()='Sub-Total:']/parent::td/following-sibling::td"));
+        WebElement total2 = driver.findElement(By.xpath("//*[text()='Total:']/parent::td/following-sibling::td"));
+        String LastsubTotalprice = subTotal2.getText().toString();
+        String lasttotalprice = total2.getText().toString();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//strong[text()='Flat Shipping Rate:']/parent::td/following-sibling::td")));
+        WebElement anotherShipping = driver.findElement(By.xpath("//strong[text()='Flat Shipping Rate:']/parent::td/following-sibling::td"));
+        String anotherFlatShipping = anotherShipping.getText().toString();
+        if(FirstsubTotalprice.equalsIgnoreCase(LastsubTotalprice)) {
+            System.out.println("subtotal is same");
+        } else {
+            System.out.println("subtotal is not same");
+        }
+        if(firsttotalprice.equalsIgnoreCase(lasttotalprice)) {
+            System.out.println("total is same");
+        } else {
+            System.out.println("total is not same");
+        }
+        if(flatShipping.equalsIgnoreCase(anotherFlatShipping)) {
+            System.out.println("flat shipping is same");
+        } else {
+            System.out.println("flat shipping is not same");
+        }
+        WebElement confirm = driver.findElement(By.cssSelector("input[value='Confirm Order']"));
+        confirm.click();
+        WebElement account = driver.findElement(By.className("fa-user"));
+        account.click();
+        WebElement history = driver.findElement(By.cssSelector("a[href$='order']"));
+        history.click();
+    }
+
+    @Test
+
+    public void  task5() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.location= 'http://tutorialsninja.com/demo/index.php?route=account/order'");
+        WebElement status = driver.findElement(By.xpath("//*[text()='Status']/parent::tr/parent::thead/following-sibling::tbody/tr/td[@class='text-left'][2]"));
+        String statusText = status.getText().toString();
+        String expectedstatusText = "Pending";
+        if(expectedstatusText.equalsIgnoreCase(statusText)) {
+            System.out.println("its pending condition");
+        } else {
+            System.out.println("its not pending condition");
+        }
+        WebElement dateElement = driver.findElement(By.xpath("//*[text()='Status']/parent::tr/parent::thead/following-sibling::tbody/tr/td[@class='text-left'][3]"));
+        String dateText = dateElement.getText().toString();
+        Date dateObject = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat ("dd/MM/yyyy");
+        String date = ft.format(dateObject).toString();
+        if(dateText.equals(date)) {
+            System.out.println("its same date");
+        } else {
+            System.out.println("its not same date");
+        }
+    }
 
 }
